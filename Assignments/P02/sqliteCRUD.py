09605792e58bd3e9271605717601b4156ab480e8
marcos <7751091+mdl0100@users.noreply.github.com>
2023-10-10 17:04:45 -0500

    ls works

--- Assignments/P02/sqliteCRUD.py
+++ Assignments/P02/sqliteCRUD.py
@@ -3,7 +3,8 @@
 
 import sqlite3
 from prettytable import PrettyTable
-
+from rich import print
+from random import randint
 class SQLiteCrud:
     """"
     SQLiteCrud Class
@@ -17,11 +18,13 @@ class SQLiteCrud:
         cursor (sqlite3.Cursor): Cursor for the database.
     """
 
-    def __init__(self, db_path):
+    def __init__(self, db_path, table_name=None):
         """Initialize database connection and cursor."""
         self.db_path = db_path
         self.conn = sqlite3.connect(self.db_path)
         self.cursor = self.conn.cursor()
+        self.table_name = table_name
+        self.columns = self.get_columns(self.table_name)
 
     def __raw_results(self, results):
         """Converts raw results to a list of table names."""
@@ -36,7 +39,99 @@ class SQLiteCrud:
         table.field_names = [desc[0] for desc in self.cursor.description]
         table.add_rows(results)
         return table
+
+    def get_columns(self, table_name):
+        """
+            Get the columns of a table.
+
+            Args:
+                table_name (str): Name of the table to get columns from.
+        """
+        self.cursor.execute(f"PRAGMA table_info({table_name});")
+        results = self.cursor.fetchall()
+        columns = []
+
+        for column_info in results:
+            column_name = column_info[1]
+            columns.append(column_name)
+        self.columns = columns
+        return columns
     
+    def insert_data(self, table_name, data):
+        """
+            Inserts data in a table.
+
+            Args:
+                table_name (str): Name of the table to insert data into.
+                data (tuple): Tuple of values to insert into the table.
+
+        """
+        try:
+            # Retrieve all data from the table
+            placeholders = ", ".join(["?"] * len(data))
+            insert_query = f"INSERT INTO {table_name} VALUES ({placeholders});"
+            self.cursor.execute(insert_query, data)
+            self.conn.commit()
+            print(f"Inserted data into table '{table_name}'.")
+        except sqlite3.Error as e:
+            print(f"Error retrieving data: {e}")
+ 
+    def close_connection(self):
+        """Close the connection to the database."""
+        self.conn.close()
+        print("Database connection closed.")
+
+    def formmated_print(self, table_name):
+        """
+            Print the contents of a table in a formatted way.
+        
+            Args:
+                table_name (str): Name of the table to print.
+        """
+        self.cursor.execute(f"SELECT * FROM {table_name};")
+        table_info = self.cursor.fetchall()
+
+        table_info_list = []
+
+        table = PrettyTable()
+        table.field_names = [desc[0] for desc in self.cursor.description]
+        table.add_rows(table_info)
+        return table
+    
+    def table_exists(self, table_name, db_path = None):
+        """
+            Checks if a table exists in the database.
+
+            Args:
+                table_name (str): Name of the table to check.
+                db_path (str): Path to the database.
+        """
+        different_conn = False
+        if not db_path:
+            db_path = self.db_path
+            conn = sqlite3.connect(db_path)
+            cursor = conn.cursor()
+        else:
+            different_conn = True
+            conn = self.conn
+            cursor = self.cursor()
+
+        try:
+            # Query the sqlite_master table to check if the table exits
+            cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+            result = cursor.fetchone()
+
+            # If result is not None, the table exists
+            return result is not None
+        
+        except sqlite3.Error as e:
+            print(f"Error checking if table exists: {e}")
+            return False
+        finally:
+            if different_conn:
+                conn.close()
+                print("Database connection closed.")
+ 
     def create_table(self, table_name, columns):
         """
             Creats a table in the database.
@@ -65,6 +160,81 @@ class SQLiteCrud:
             print(f"Table '{table_name}' created.")
         except sqlite3.Error as e:
             print(f"Error creating table: {e}")
+    
+    def read_data(self, table_name, condition_column=None, condition_value=None, order_by=None, order="ASC"):
+        """
+            Reads data from a table.
+
+            Args:
+                table_name (str): Name of the table to read data from.
+        """
+        try:
+            if order_by:
+                order = f"ORDER BY {order_by} {order}"
+            else:
+                order = ""
+            
+            if condition_column and condition_value:
+                # Retrieve all data from the table
+                select_query = f"SELECT * FROM {table_name} WHERE {condition_column} = '{condition_value}' {order};"
+                self.cursor.execute(select_query)
+                results = self.cursor.fetchall()
+                print(f"Retrieved data from table '{table_name}'.")
+                output = []
+                for row in results:
+                    output.append(dict(zip(self.columns, row)))
+                return output
+            else:
+                # Retrieve all data from the table
+                select_query = f"SELECT * FROM {table_name} {order};"
+                self.cursor.execute(select_query)
+                results = self.cursor.fetchall()
+                print(f"Retrieved data from table '{table_name}'.")
+                output = []
+                for row in results:
+                    output.append(dict(zip(self.columns, row)))
+                return output
+        except sqlite3.Error as e:
+            print(f"Error retrieving data: {e}")
+
+    def update_data(self, table_name, column, new_value, condition_column, condition_value):
+        """
+            Updates data in a table based on a condition.
+
+            Args:
+                table_name (str): Name of the table to update data in.
+                column (str): Name of the column to update.
+                new_value (str): New value to update the column with.
+                condition_column (str): Name of the column to use in the condition.
+                condition_value (str): Value to use in the condition.
+        """
+        try:
+            # Update the data in the table based on the condition
+            update_query = f"UPDATE {table_name} SET {column} = '{new_value}' WHERE {condition_column} = '{condition_value}';" 
+            self.cursor.execute(update_query)
+            self.conn.commit()
+            print(f"Updated data in table '{table_name}'.")
+        except sqlite3.Error as e:
+            print(f"Error updating data: {e}")
+
+    def delete_data(self, table_name, condition_column, condition_value):
+        """
+            Deletes data in a table based on a condition.
+
+            Args:
+                table_name (str): Name of the table to delete data from.
+                condition_column (str): Name of the column to use in the WHERE clause.
+                condition_value (str): Value to use in the WHERE clause.
+        """
+
+        try:
+            # Delete the data from the table based on the condition
+            delete_query = f"DELETE FROM {table_name} WHERE {condition_column} = '{condition_value}';"
+            self.cursor.execute(delete_query)
+            self.conn.commit()
+            print(f"Deleted data from table '{table_name}'.")
+        except sqlite3.Error as e:
+            print(f"Error deleting data: {e}")
 
     def drop_table(self, table_name):
         """
@@ -135,188 +305,56 @@ class SQLiteCrud:
                 table.append({f"Column Name: {column_name}, Data Type: {data_type}, Nullable: {if_nullable}"})
         return table
     
-    def insert_data(self, table_name, data):
-        """
-            Inserts data in a table.
-
-            Args:
-                table_name (str): Name of the table to insert data into.
-                data (tuple): Tuple of values to insert into the table.
-
-        """
-        try:
-            # Retrieve all data from the table
-            placeholders = ", ".join(["?"] * len(data))
-            insert_query = f"INSERT INTO {table_name} VALUES ({placeholders});"
-            self.cursor.execute(insert_query, data)
-            self.conn.commit()
-            print(f"Inserted data into table '{table_name}'.")
-        except sqlite3.Error as e:
-            print(f"Error retrieving data: {e}")
-
-    def read_data(self, table_name, condition_column=None, condition_value=None):
-        """
-            Reads data from a table.
-
-            Args:
-                table_name (str): Name of the table to read data from.
-        """
-        try:
-            if condition_column and condition_value:
-                # Retrieve all data from the table
-                select_query = f"SELECT * FROM {table_name} WHERE {condition_column} = '{condition_value}';"
-                self.cursor.execute(select_query)
-                results = self.cursor.fetchall()
-                print(f"Retrieved data from table '{table_name}'.")
-                return self.__formatted_results(results)
-            else:
-                # Retrieve all data from the table
-                select_query = f"SELECT * FROM {table_name};"
-                self.cursor.execute(select_query)
-                results = self.cursor.fetchall()
-                print(f"Retrieved data from table '{table_name}'.")
-                return self.__formatted_results(results)
-        except sqlite3.Error as e:
-            print(f"Error retrieving data: {e}")
-
-    def update_data(self, table_name, column, new_value, condition_column, condition_value):
-        """
-            Updates data in a table based on a condition.
-
-            Args:
-                table_name (str): Name of the table to update data in.
-                column (str): Name of the column to update.
-                new_value (str): New value to update the column with.
-                condition_column (str): Name of the column to use in the condition.
-                condition_value (str): Value to use in the condition.
-        """
-        try:
-            # Update the data in the table based on the condition
-            update_query = f"UPDATE {table_name} SET {column} = '{new_value}' WHERE {condition_column} = '{condition_value}';" 
-            self.cursor.execute(update_query)
-            self.conn.commit()
-            print(f"Updated data in table '{table_name}'.")
-        except sqlite3.Error as e:
-            print(f"Error updating data: {e}")
-
-    def delete_data(self, table_name, condition_column, condition_value):
-        """
-            Deletes data in a table based on a condition.
-
-            Args:
-                table_name (str): Name of the table to delete data from.
-                condition_column (str): Name of the column to use in the WHERE clause.
-                condition_value (str): Value to use in the WHERE clause.
-        """
-
-        try:
-            # Delete the data from the table based on the condition
-            delete_query = f"DELETE FROM {table_name} WHERE {condition_column} = '{condition_value}';"
-            self.cursor.execute(delete_query)
-            self.conn.commit()
-            print(f"Deleted data from table '{table_name}'.")
-        except sqlite3.Error as e:
-            print(f"Error deleting data: {e}")
-
-    def close_connection(self):
-        """Close the connection to the database."""
-        self.conn.close()
-        print("Database connection closed.")
-
-    def formmated_print(self, table_name):
-        """
-            Print the contents of a table in a formatted way.
-        
-            Args:
-                table_name (str): Name of the table to print.
-        """
-        self.cursor.execute(f"SELECT * FROM {table_name};")
-        table_info = self.cursor.fetchall()
-
-        table_info_list = []
-
-        table = PrettyTable()
-        table.field_names = [desc[0] for desc in self.cursor.description]
-        table.add_rows(table_info)
-        return table
-    
-    def table_exists(self, table_name, db_path = None):
-        """
-            Checks if a table exists in the database.
-
-            Args:
-                table_name (str): Name of the table to check.
-                db_path (str): Path to the database.
-        """
-        different_conn = False
-        if not db_path:
-            db_path = self.db_path
-            conn = sqlite3.connect(db_path)
-            cursor = conn.cursor()
-        else:
-            different_conn = True
-            conn = self.conn
-            cursor = self.cursor()
-
-        try:
-            # Query the sqlite_master table to check if the table exits
-            cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
-            result = cursor.fetchone()
-
-            # If result is not None, the table exists
-            return result is not None
-        
-        except sqlite3.Error as e:
-            print(f"Error checking if table exists: {e}")
-            return False
-        finally:
-            if different_conn:
-                conn.close()
-                print("Database connection closed.")
-
-    
 if __name__ == "__main__":
-    db_name = "students.sqlite"
-    conn = SQLiteCrud(db_name)
+    db_name = "filesystem.sqlite"
+    table = "filesystem"
+    conn = SQLiteCrud(db_name, table)
 
-    # Define table schema
-    table_name = "students"
-    columns = ["id TEXT", "name TEXT", "age INTEGER"] 
+    # Get Columns
+    print("Columns: ")
+    print(conn.columns)
 
-    # Create table
-    conn.create_table(table_name, columns)
+    for file in conn.read_data(table):
+        conn.update_data(table, "file_size", randint(0, 300000), "filename", file["filename"])
 
-    # Insert data
-    data = ("1", "Alice", 25)
-    conn.insert_data(table_name, data)
+    # # Define table schema
+    # table_name = "students"
+    # columns = ["id TEXT", "name TEXT", "age INTEGER"] 
+
+    # # Create table
+    # conn.create_table(table_name, columns)
+
+    # # Insert data
+    # data = ("1", "Alice", 25)
+    # conn.insert_data(table_name, data)
     
 
-    data = ("2", "Bob", 22)
-    conn.insert_data(table_name, data)
+    # data = ("2", "Bob", 22)
+    # conn.insert_data(table_name, data)
 
-    data = ("3", "Charlie", 20)
-    conn.insert_data(table_name, data)
+    # data = ("3", "Charlie", 20)
+    # conn.insert_data(table_name, data)
 
-    # Read Data
-    conn.read_data(table_name)
+    # # Read Data
+    # conn.read_data(table_name)
 
-    # Update data
-    conn.update_data(table_name, "age", 26, "name", "Alice")
+    # # Update data
+    # conn.update_data(table_name, "age", 26, "name", "Alice")
 
-    # Display table
-    print(conn.formmated_print(table_name))
+    # # Display table
+    # print(conn.formmated_print(table_name))
 
-    # Delete data
-    conn.delete_data(table_name, "name", "Charlie")
+    # # Delete data
+    # conn.delete_data(table_name, "name", "Charlie")
 
-    # Display table
-    print(conn.formmated_print(table_name))
+    # # Display table
+    # print(conn.formmated_print(table_name))
 
-    # Drop table
-    conn.drop_table(table_name)
+    # # Drop table
+    # conn.drop_table(table_name)
 
-    # Display tables
-    print(conn.show_tables())
+    # # Display tables
+    # print(conn.show_tables())
 
     # Close connection
     conn.close_connection()
--- Assignments/P02/walkthrough.py
+++ Assignments/P02/walkthrough.py
@@ -4,18 +4,47 @@ from rich.table import Table
 from rich.box import SIMPLE
 from sqliteCRUD import SQLiteCrud
 
-def display_ls(files):
+
+def display_ls(files, long=False, hidden=False, readable=False):
     table = Table(show_header=True, header_style="bold blue", box=SIMPLE)
-    table.add_column("Permissions", style="dim", width=12)
-    table.add_column("Owner", width=10)
-    table.add_column("Owner_Group", width=10)
-    table.add_column("Size", justify="right")
-    table.add_column("Last Modified", justify="right")
-    table.add_column("Filename")
+    if not long:
+        table.add_column("Filename")
+        for row in files:
+            table.add_row(row["filename"])
+    else:
+        table.add_column("Permissions", style="dim", width=12)
+        table.add_column("Owner", justify="center")
+        table.add_column("Owner_Group", justify="center")
+        table.add_column("File_size", justify="right")
+        table.add_column("Last Modified", justify="right")
+        table.add_column("Filename")
+        if readable:
+            for row in files:
+                row['file_size'] = convert_size(row['file_size'])
+        
+        if hidden:
+            for row in files:
+                if row['hidden']:
+                    table.add_row(
+                        row["permissions"],
+                        row["owner"],
+                        row["group_name"],
+                        str(row["file_size"]),
+                        row["modification_time"],
+                        row["filename"]
+                    )
+        for row in files:
+            if not row['hidden']:
+                table.add_row(
+                    row["permissions"],
+                    row["owner"],
+                    row["group_name"],
+                    str(row["file_size"]),
+                    row["modification_time"],
+                    row["filename"]
+                )
 
 
-    for filename, (owner, perms, size) in files.items():
-        table.add_row(filename, owner, perms, size)
     print(table)
     time.sleep(10)
 
@@ -23,17 +52,41 @@ def display_pwd(directory):
     print(f"[bold cyan]Current Directory:[/bold cyan] [green]{directory}[/green]")
     time.sleep(1)
 
+def convert_size(size):
+    if size:
+        for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+            if size < 1024.0:
+                break
+            size /= 1024.0
+        return f"{size:.1f}{unit}"
+    return size
+
+
 if __name__ == "__main__":
     # Create the database
-    db = SQLiteCrud("filesystem.sqlite")
-    table = "filesystem"
+    database = "filesystem.sqlite"
+    table = "filesystem"    
+    db = SQLiteCrud("filesystem.sqlite", table)
+
+    # Create the filesystem table
+
+    check = db.read_data("filesystem", order_by="filename")
+    # print(check)
     
-    check = db.read_data("filesystem")
-    print(db.__raw_results(check))
-    
-    # # Demonstrate the commands
-    # print("[bold blue]Command:[/bold blue] [green]ls[/green]")
-    # display_ls(table)
+    # Demonstrate the commands
+    print("[bold blue]Command:[/bold blue] [green]ls[/green]")
+    display_ls(check, long=False)
+
+    print("[bold blue]Command:[/bold blue] [green]ls -l[/green]")
+    display_ls(check, long=True)
+
+    print("[bold blue]Command:[/bold blue] [green]ls -la[/green]")
+    display_ls(check, long=True, hidden=True)
+
+    print("[bold blue]Command:[/bold blue] [green]ls -lah[/green]")
+    display_ls(check, long=True, hidden=True, readable=True)
+
+
 
     # print("\n[bold blue]Command:[/bold blue] [green]mkdir newfolder[/green]")
     # print("[bold green]Folder 'newfolder' created.[/bold green]")
@@ -59,3 +112,4 @@ if __name__ == "__main__":
 
     # print("\n[bold blue]Command:[/bold blue] [green]pwd[/green]")
     # display_pwd(current_directory)
+
